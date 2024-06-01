72de578dad81f019a83b060c8bc558d2ea89338d
duchadev <95076379+hannguu@users.noreply.github.com>
2024-06-01 15:37:54 +0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -14,26 +14,43 @@ connect.then((db) => {
     //     description: 'test'
     // });
 
-        Dishes.create({
-               name: 'Uthapizza',
-                description: 'Test'
-        	    })
-        	    .then((dish) => {
-    	        console.log(dish);
-        	        
-        	        return Dishes.find({}).exec();
-        	    })
-        	    .then((dishes) => {
-        	        console.log(dishes);
-        	
-        	        return Dishes.deleteMany({});
-            })
-        	    .then(() => {
-        	        return mongoose.connection.close();
-        	    })
-        	    .catch((err) => {
-        	        console.log(err);
-        	    });
-        
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+
 
 });
